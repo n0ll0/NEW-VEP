eae46eb71ea81fb62ffc89d6eeea044b445de603
n0ll0 <101214789+n0ll0@users.noreply.github.com>
2022-10-25 19:32:35 +0300

    PHP full project

---
+++ form.php
@@ -0,0 +1,19 @@
+<?php 
+use Dotenv\Dotenv;
+require __DIR__.'/vendor/autoload.php';
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+$client = new MongoDB\Client($_ENV['MONGODB']);
+$collection = $client->test->support;
+
+$collection->insertOne([
+  "name"=> $name, 
+  "email"=> $email,
+  "message"=> $message
+]);
+
